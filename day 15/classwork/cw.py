b7fe62349d4a4b4b915363f2d457ad6851db6f97
Demetre05 <demetre.gh@gmail.com>
2025-11-22 23:51:59 +0400

    commit

---
+++ day 15/classwork/cw.py
@@ -0,0 +1,19 @@
+num = 100
+
+while num <= 200:
+    print(num)
+    num += 1
+
+num1 = 0
+
+while num1 < 20:
+    print(num1)
+    num1 += 2
+
+
+name1 = input("enter your name: ")
+
+while name1 != "demetre":
+    print("")
+    name1 = input("enter your name: ")
+print("this is my name")
