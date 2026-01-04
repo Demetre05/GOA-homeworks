aa5924d0a3062285586f61594484bd8301884e33
Demetre05 <demetre.gh@gmail.com>
2026-01-04 23:01:04 +0400

    commit

---
+++ day 24/classwork/cw.py
@@ -0,0 +1,26 @@
+names = ["giorgi", "nika", "sandro", "dato", "andria"]
+
+print(names)
+print(names[2])
+print(names[3])
+
+names[4] = "demetre"
+print(names)
+
+names[1], names[2] = names[2], names[1]
+print(names)
+
+print(names[-2])
+
+
+
+
+
+
+
+names1 = ["vaso", "vaja", "cotne", "lazare", "mate", "vano", "saba"]
+print(len(names1))
+
+
+name = input("enter your name: ")
+print(len(name))
