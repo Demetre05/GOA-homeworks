a2108af167c995192f9b0490bd1a93b32a848d61
Demetre05 <demetre.gh@gmail.com>
2026-01-17 23:39:07 +0400

    commit

---
+++ day 28/classwork/cw.py
@@ -0,0 +1,15 @@
+nums = [15, 7, 5, 21, 8, 19, 3, 4]
+
+print(nums[0])
+print(nums[-1])
+print(nums[:3])
+print(nums[-2:])
+print(nums[1:-1])
+print(nums[::-1])
+print(nums[1])
+print(nums[1::2])
+
+nums1 = nums[:-1]
+print(nums1[3])
+
+print(nums[:4])
