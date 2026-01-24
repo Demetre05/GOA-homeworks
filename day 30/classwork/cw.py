1449229632fa5301a176fe893fe40fb79b7ca9dd
Demetre05 <demetre.gh@gmail.com>
2026-01-24 23:07:59 +0400

    commit

---
+++ day 30/classwork/cw.py
@@ -0,0 +1,15 @@
+string = "hELlo woRld"
+
+print(string.upper())
+print(string.lower())
+print(string.capitalize())
+print(string.title())
+print(string.find("w"))
+print(string.find("l", 4))
+print(string.find("c"))
+print(string.find("woRld"))
+
+
+# find ფუნქცია საჭიროა სტრინგში გარკვეული სიმბოლოს ან მათი გარკვეული თანმიმდევრობით მითითებული ერთობლიობის მოსაძებნად.
+# find-ის პირველ არგუმენტში ვწერთ, თუ რა სიმბოლო ან მათი ერთობლიობა უნდა მოძებნოს, ხოლო მეორე არგუმენტში ვწერთ, თუ რომელი
+# ინდექსიდან უნდა დაიწყოს ძებნა.
