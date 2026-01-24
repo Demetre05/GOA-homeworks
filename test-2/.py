899c176d098271421987722d7a0e6df56b0dbbae
Demetre05 <demetre.gh@gmail.com>
2026-01-24 23:56:36 +0400

    commit

---
+++ test-2/.py
@@ -0,0 +1,47 @@
+# num = int(input("enter number: "))
+
+# if num % 2 == 0:
+#     print("this number is even")
+# else:
+#     print("this number is odd")
+
+
+
+
+
+# xmovani = ["a", "e", "y", "u", "i", "o"]
+
+
+# word = input("enter some word: ")
+# count = 0
+
+# for i in word:
+#     if i in xmovani:
+#         count += 1
+
+# print(count)
+
+
+
+
+
+# name = input("enter your name: ")
+
+# print(name[1:-1])
+
+
+
+# name = input("enter your name: ")
+
+# print(name[::-1])
+
+
+
+
+
+# name = input("enter your name: ")
+# num = int(input("enter number: "))
+
+
+# for i in range(num):
+#     print(name)
