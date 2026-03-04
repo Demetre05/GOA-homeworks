399dc67ac9e85e53dffc7923d696b4967eb4c123
Demetre05 <demetre.gh@gmail.com>
2026-03-04 22:59:15 +0400

    commit

--- .py
+++ .py
@@ -1,8 +1,8 @@
 # def my_find(word, letters):
-#     for i in range(len(word)):
-#         if word[i:i+len(letters)] == letters:
-#             return i
-#     return -1
+#      for i in range(len(word)):
+#          if word[i:i+len(letters)] == letters:
+#              return i
+#      return -1
 # print(my_find("hello", "el"))
 
 #--------------------------------------------------------------------------------------------------------------------------------------
