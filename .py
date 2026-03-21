c771e1bf97102e70f9320811b5b55593203cfb27
Demetre05 <demetre.gh@gmail.com>
2026-03-21 23:31:21 +0400

    commit

--- .py
+++ .py
@@ -1,33 +1,15 @@
-# def my_find(word, letters):
-#      for i in range(len(word)):
-#          if word[i:i+len(letters)] == letters:
-#              return i
-#      return -1
-# print(my_find("hello", "el"))
-
-#--------------------------------------------------------------------------------------------------------------------------------------
-
-# list = [4, 6, 8, 7, 5, 9]
-
-# list.remove(6)
-# print(list)
+# def candles(candles_number, make_new):
+#     remaining = candles_number - make_new * (candles_number // make_new) # 1
+#     while remaining != 0: # 1 != 0
+#         candles_number += candles_number // make_new # 7
+#         remaining += candles_number - make_new * (candles_number // make_new) # 2
+#         if remaining >= make_new: # 2 >= 2
+#             candles_number += remaining // make_new
+#             remaining = 0
+#     return candles_number + 1
 
 
-def candles(candles_number, make_new):
-    remaining = candles_number - make_new * (candles_number // make_new) # 1
-    while remaining != 0: # 1 != 0
-        candles_number += candles_number // make_new # 7
-        remaining += candles_number - make_new * (candles_number // make_new) # 2
-        if remaining >= make_new: # 2 >= 2
-            candles_number += remaining // make_new
-            remaining = 0
-    return candles_number + 1
-
-
-print(candles(5, 2))
-
-
-
+# print(candles(5, 2))
 
 
 
@@ -43,3 +25,6 @@ print(candles(5, 2))
 #             candles_number += remaining // make_new
 #             remaining - 
 #     return candles_number
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
