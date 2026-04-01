219888b406d7f57a67058154695ffb51f78c382e
Demetre05 <demetre.gh@gmail.com>
2026-04-01 18:14:50 +0400

    commit

--- .py
+++ .py
@@ -1,30 +1,52 @@
-# def candles(candles_number, make_new):
-#     remaining = candles_number - make_new * (candles_number // make_new) # 1
-#     while remaining != 0: # 1 != 0
-#         candles_number += candles_number // make_new # 7
-#         remaining += candles_number - make_new * (candles_number // make_new) # 2
-#         if remaining >= make_new: # 2 >= 2
-#             candles_number += remaining // make_new
-#             remaining = 0
-#     return candles_number + 1
+def is_anagram(test, original):
+
+    l_test = test.lower()
+    list = []
+
+    l_original = original.lower()
+    list1 = []
+
+    for i in l_test:
+        list.append(i)
+
+    for i in l_original:
+        list1.append(i)
+
+    if list in list1:
+        return True
+    return False
+
+# print(is_anagram("hello", "olleh"))
 
 
-# print(candles(5, 2))
+
+
+def anagram(a, b):
+    if a in b:
+        return False
+    return True
+
+# print(anagram("hello", "olleh"))
 
 
 
-# def candles(candles_number, make_new): # (5, 2)
-#     remaining = candles_number - make_new * (candles_number // make_new) # 1
-#     while remaining != 0: # 1 != 0
-#         candles_number += candles_number // make_new # 7
-#         remaining += candles_number - make_new * (candles_number // make_new) # 2
-#         if remaining = make_new: # 2 >= 2
-#             candles_number += 1
-#             remaining = 0
-#         elif remaining > make_new:
-#             candles_number += remaining // make_new
-#             remaining - 
-#     return candles_number
 
-#------------------------------------------------------------------------------------------------------------------------------------
+if "hello" in "helol":
+    print(True)
+else:
+    print(False)
 
+
+
+def anagram(a1, b1):
+    c = 0
+    a = a1.lower()
+    b = b1.lower()
+    for i in a:
+        if i in b:
+            c += 1
+    if len(b) == 1:
+        return False
+    elif c == len(b):
+        return True
+    return False
