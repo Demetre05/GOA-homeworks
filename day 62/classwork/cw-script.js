d479d3434733e42e7fde7ac975ebfe5b5c2b829e
Demetre05 <demetre.gh@gmail.com>
2026-05-13 23:08:00 +0400

    commit

---
+++ day 62/classwork/cw-script.js
@@ -0,0 +1,19 @@
+// number - ნებისმიერი რიცხვი
+// bigint - ნებისმიერი რიცხვი ნაკლები -2**53 ან მეტი 2**53
+// string - ბრჭყალებში ჩასმული ნებისმიერი რამ
+// boolean - true/false
+// null - არარსებული მონაცემი
+// undefined - არარსებული მნიშვნელობა
+// symbol - ნებისმიერი სიმბოლო. მაგ: @#$^&*
+
+console.log(5 + 3)
+console.log(5 - 3)
+console.log(5 * 3)
+console.log(5 / 3)
+console.log(5 % 3)
+
+console.log("hello " + "world")
+console.log("bla" + "bla")
+console.log("good " + "bye")
+
+// console - ინსტრუმენტი, რომელზეც ვხედავთ ჯავასკრიპტში დაწერილი კოდის შედეგს
