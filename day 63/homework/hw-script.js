921640b4067ca5f2c66ef50a619ad33adae98e3c
Demetre05 <demetre.gh@gmail.com>
2026-05-16 23:12:20 +0400

    commit

---
+++ day 63/homework/hw-script.js
@@ -0,0 +1,20 @@
+/*
+    ჯავასცრიპტი პროგრამირების ენად მაშინ იქცა, როდესაც მისი გამოყენება სერვერების,
+    მობილური და დესკტოპ აპლიკაციების შესაქმნელად გახდა შესაძლებელი.
+
+    null - მონაცემი, რომელიც არ არის მითითებული
+    undefined - მონაცემი, რომელიც საერთოდ არ არსებობს
+*/
+
+
+// .length - ტექსტის სიგრძეს განსაზღვრავს
+
+console.log("Hello World".length);
+console.log("Hello JS".length);
+console.log("idk what to say".length);
+
+// .toUpperCase() - ტექსტის ყველა ასო დიდ ასოებში გადაჰყავს
+
+console.log("Hello World".toUpperCase());
+console.log("Hello JS".toUpperCase());
+console.log("idk what to say".toUpperCase());
