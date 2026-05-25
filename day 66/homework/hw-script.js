9c027ecf2453f268ca1952288ccbc6de8f721d12
Demetre05 <demetre.gh@gmail.com>
2026-05-25 20:51:59 +0400

    commit

---
+++ day 66/homework/hw-script.js
@@ -0,0 +1,23 @@
+console.log(5 + 1);
+console.log(5 - 2);
+console.log(5 * 3);
+console.log(5 / 4);
+
+console.log(typeof "hello");
+console.log(typeof true);
+console.log(typeof 4n);
+console.log(typeof 5);
+console.log(typeof n);
+
+let num = 5
+console.log(num += 2);
+console.log(num -= 3);
+console.log(num *= 4);
+console.log(num /= 5);
+num++
+console.log(num);
+num--
+console.log(num);
+
+console.log("Hello " + "World");
+console.log("How " + "are " + "you" + "?");
