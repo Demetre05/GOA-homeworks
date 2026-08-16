1bfad24ff061ab2d02d1825e441d4f0cecbaa7cb
Demetre05 <demetre.gh@gmail.com>
2026-08-16 17:50:31 +0400

    commit

---
+++ day 087/classwork/cw-script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector("#container");
+
+const contList = container.children;
+for(let i = 0; i < contList.length; i++) {
+    contList[i].className = "card";
+    contList[i].id = "card" + (i + 1);
+};
+
+const buttList = document.querySelectorAll("button");
+for(let i = 0; i < buttList.length; i++) {
+    buttList[i].className = "butt";
+    buttList[i].id = "butt" + (i + 1);
+};
+
+const addCard = (x) => {
+    const newDiv = document.createElement("div");
+    newDiv.className = "card";
+    newDiv.id = `card${contList.length + 1}`;
+    newDiv.textContent = "ახალი ბარათი";
+    newDiv.style.display = "linear";
+    newDiv.style.backgroundColor = "yellow";
+    return x.appendChild(newDiv);
+};
+
+const changeCard = (x) => {
+    for(let i = 0; i < x.length; i++)
+    x[i].style.backgroundColor = "green";
+};
+
+const removeCard = (x) => x.remove();
