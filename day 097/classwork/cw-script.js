c8f1896cc570747678ace5b8a0f5cedda6943d77
Demetre05 <demetre.gh@gmail.com>
2026-09-20 23:10:59 +0400

    commit

---
+++ day 097/classwork/cw-script.js
@@ -0,0 +1,26 @@
+const student = {
+    name: "Nika",
+    age: 20,
+    skills: ["HTML", "CSS", "JavaScript"],
+    city: "Tbilisi"
+};
+
+const {name, age} = student;
+console.log(name, age);
+
+for(const key in student) {
+    console.log(key);
+    console.log(student[key]);
+};
+
+const {skills} = student;
+for(const el of skills) {
+    console.log(el);
+};
+
+const showSkills = (first, ...otherSkills) => {
+    console.log(first);
+    console.log(otherSkills);
+}
+
+showSkills(skills[0], skills[1], skills[2]);
